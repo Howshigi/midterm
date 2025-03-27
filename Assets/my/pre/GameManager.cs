835cf46324752fb2cfff085f5288e5ac7a8f2fc2
WatTunTo <nipat523naruto@gmail.com>
2025-03-28 00:52:09 +0700

    UI เกือบเสร็จ เหลือ Code Void Update

--- Assets/my/pre/GameManager.cs
+++ Assets/my/pre/GameManager.cs
@@ -19,17 +19,66 @@ public class GameManager : MonoBehaviour
     public GameObject titleScreen;
     public GameObject gameOverScreen;
     public Button easyButton;
-    public Button mediumButton;
+    public Button NormalButton;
     public Button hardButton;
+    
+    private int score;
+    private bool isGameActive = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    void Awake()
+    {
+        easyButton.onClick.AddListener(() =>
+        {
+            StartGame(1.0f);
+        });
+        
+        NormalButton.onClick.AddListener(() =>
+        {
+            StartGame(1.5f);
+        });
+        
+        hardButton.onClick.AddListener(() =>
+        {
+            StartGame(2.0f);
+        });
+    }
+    
     void Start()
     {
-        
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    
+    
+    
+    
+    void StartGame(float difficulty)
+    {
+        titleScreen.SetActive(false);
+        
+    }
+    
+    public void UpdateScore(int score)
+    {
+        this.score += score;
+        scoreText.text = this.score.ToString();
+    }
+    
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
+        isGameActive = false;
+    }
+    public void RestartGame()
+    {
+        //SceneManager.LoadScene("Main");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
